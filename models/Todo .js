a2c090b764b8220df1051c14fb7f49c4e6259952
thsc47 <thsc47@gmail.com>
2021-09-11 12:00:06 -0300

    Todo Schema finished

--- models/Todo .js
+++ models/Todo .js
@@ -1,1 +1,13 @@
-const mongoose = require('mongoose')
+const { Schema, model } = require("mongoose");
+
+const TodoSchame = new Schema(
+  {
+    title: { type: String, required: true },
+    completed: { type: Boolean, default: false },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = model('Todo', TodoSchame);
