17c6c1e238da5a99fe6f647248b17ce3fd3c434d
thsc47 <thsc47@gmail.com>
2021-09-11 12:17:53 -0300

    Route POST Done

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const express = require("express");
-const dbConnect = require('./config/db.config');
+const dbConnect = require("./config/db.config");
+const Todo = require("./models/Todo");
+
 dbConnect();
 
 const app = express();
 
-app.listen(5000, ()=>{console.log("Server Running at port 5000")})
+app.use(express.json());
 
+app.post("/new-task", async (req, res) => {
+  try {
+    const newTask = await Todo.create(req.body);
+    res.status(201).json(newTask);
+  } catch (error) {
+    res.status(500).json({ msg: 'Error on create a new Todo', error });
+  }
+});
+app.listen(5000, () => {
+  console.log("Server Running at port 5000");
+});
