1bfe4e35fbea4cd36faf6b86ef94085f6476846f
thsc47 <thsc47@gmail.com>
2021-09-11 12:45:51 -0300

    Route POST  Done

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.get("/todos", async (req, res) => {
         const allTasks = await Todo.find();
         res.json(allTasks);
     } catch (error) {
-        
+        res.status(500).json({ msg: 'Error on find all Todos', error });
     }
 })
 
@@ -25,6 +25,18 @@ app.post("/todos", async (req, res) => {
     res.status(500).json({ msg: 'Error on create a new Todo', error });
   }
 });
+
+app.put("/todos/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const body = req.body;
+        const updatedTask = await Todo.findOneAndUpdate({_id: `${id}`}, body, {new: true})
+        res.status(200).json({msg: `The ${id} was updated`})
+    } catch (error) {
+        res.status(500).json({ msg: 'Error on update the record', error });
+    }
+})
+
 app.listen(5000, () => {
   console.log("Server Running at port 5000");
 });
